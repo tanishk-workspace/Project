fbf391ad57b295862d07893d0b3e6d16cdb5039f
tanishk <137615344+tanishk-workspace@users.noreply.github.com>
2025-03-31 04:28:40 +0530

    Update project.ino

--- project.ino
+++ project.ino
@@ -1,21 +1,34 @@
 #define irsensorpin 2
 #include<Chrono.h>
 Chrono mychrono(Chrono::SECONDS);
+int rpm[2]={0,rpm};
+float d=1.0;
 void setup() {
   pinMode(irsensorpin, INPUT);
   Serial.begin(9600);
+  run=false;
+}
+void update_rpm(){
+  if(run==true){
+    rpm[1]=rpm[0];
+    rpm[0]=60/time;
+    if(rpm[0]=rpm[1]){
+      Serial.println("rpm :",rpm,'\n');
+    }
+  }
 }
 void loop() {
   int Sensorvalue = digitalRead(irsensorpin);
-  mychrono.resume();
-  if(Sensorvalue = HIGH){
+  mychrono.restart();
+  if(Sensorvalue = LOW){
+    while(Sensorvalue=HIGH){
     int time=mychrono.elapsed();
     Serial.println(time,'\n');
+    }
   }
-  else{
-    mychrono.stop();
-  }
-
-  //display rpm delay 1000ms
-  //else there will be false positive error
+  mychrono.stop();
+  update_rpm();
 }
+
+//you should probably revert back to millis
+//LED_indicators(){case(rpm):}
